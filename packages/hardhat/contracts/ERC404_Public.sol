c143df017a829f2634fa1213d2a9068c62b339af
franc2es <no1pengwu@163.com>
2024-05-18 18:00:24 +0800

    ✨ update contract

--- packages/hardhat/contracts/ERC404_Public.sol
+++ packages/hardhat/contracts/ERC404_Public.sol
@@ -3,16 +3,13 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC404_Public is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
-	uint256 private _nextTokenId;
-
+contract ERC404_Public is ERC721, ERC721Pausable, Ownable {
 	constructor(
 		address initialOwner
-	) ERC721("ERC404_Public", "MTK") Ownable(initialOwner) {}
+	) ERC721("ERC404_Public", "ERC404_Public") Ownable() {}
 
 	function pause() public onlyOwner {
 		_pause();
@@ -22,35 +19,14 @@ contract ERC404_Public is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
 		_unpause();
 	}
 
-	function safeMint(address to) public onlyOwner {
-		uint256 tokenId = _nextTokenId++;
-		_safeMint(to, tokenId);
-	}
-
 	// The following functions are overrides required by Solidity.
 
-	function _update(
+	function _beforeTokenTransfer(
+		address from,
 		address to,
-		uint256 tokenId,
-		address auth
-	)
-		internal
-		override(ERC721, ERC721Enumerable, ERC721Pausable)
-		returns (address)
-	{
-		return super._update(to, tokenId, auth);
-	}
-
-	function _increaseBalance(
-		address account,
-		uint128 value
-	) internal override(ERC721, ERC721Enumerable) {
-		super._increaseBalance(account, value);
-	}
-
-	function supportsInterface(
-		bytes4 interfaceId
-	) public view override(ERC721, ERC721Enumerable) returns (bool) {
-		return super.supportsInterface(interfaceId);
+		uint256 firstTokenId,
+		uint256 batchSize
+	) internal virtual override(ERC721, ERC721Pausable) {
+		super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
 	}
 }
--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,14 +1,14 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
+// import { Contract } from "ethers";
 
 /**
- * Deploys a contract named "YourContract" using the deployer account and
+ * Deploys a contract named "ERC404_Public" using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployERC404_Public: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -22,7 +22,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("ERC404_Public", {
     from: deployer,
     // Contract constructor arguments
     args: [deployer],
@@ -33,12 +33,11 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  // const ERC404_Public = await hre.ethers.getContract<Contract>("ERC404_Public", deployer);
 };
 
-export default deployYourContract;
+export default deployERC404_Public;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+// e.g. yarn deploy --tags ERC404_Public
+deployERC404_Public.tags = ["ERC404_Public"];
